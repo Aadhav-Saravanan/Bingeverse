b70beec39d191736bce338054d9defda9e776259
Aadhav Saravanan <119620882+Aadhav-Saravanan@users.noreply.github.com>
2023-07-10 18:49:48 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
       },
+      letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+      }
     },
   },
   plugins: [],
